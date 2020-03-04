4eb1995bf82c13cd8e01042d5702fbaece717377
UbaidurRehman1 <ubaidshan007@gmail.com>
2020-03-04 14:02:48 +0500

    added

---
+++ GuestTracker/src/main/java/com/ubaid/idealfesta/GuestTracker/GuestTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.ubaid.idealfesta.GuestTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestTrackerApplication.class, args);
+	}
+
+}
---
+++ GuestTracker/src/test/java/com/ubaid/idealfesta/GuestTracker/GuestTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubaid.idealfesta.GuestTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
